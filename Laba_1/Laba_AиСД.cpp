b82e61a0ee1bd5a428328ce2fc28c493bd3bc360
Zhenya Nehaychik <119797207+ZhenyaNeh@users.noreply.github.com>
2023-03-29 14:04:50 +0300

    Add files via upload

---
+++ Laba_1/Laba_AиСД.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <time.h>       
+
+using namespace std;
+
+int main()
+{   
+    int N;
+    cout << "Enter N: "; cin >> N; cout << "\n\n";
+
+    clock_t start{ clock() };
+    const int SIZE = N;
+
+    if (SIZE <= 0)
+        cout << "Error....";
+    else
+    {
+        int* fibonachiArray = new int[SIZE];
+        fibonachiArray[0] = 0;
+        fibonachiArray[1] = 1;
+        if (SIZE == 1)
+            cout << fibonachiArray[0];
+        else if (SIZE == 2)
+            cout << fibonachiArray[1];
+        else if (SIZE > 2)
+        {
+            for (int i = 2; i < SIZE; i++)
+            {
+                fibonachiArray[i] = fibonachiArray[i - 1] + fibonachiArray[i - 2];
+            }
+            cout << "Value Fibonachi: " << fibonachiArray[SIZE - 1] << "\n";
+        }
+
+        for (int j = 0; j < SIZE; j++)
+        {
+            cout << fibonachiArray[j] << " ";
+        }
+        cout << endl;
+    }
+    clock_t end{ clock() };
+    double time_spent_sec{ (double)(end - start) / CLOCKS_PER_SEC };
+
+    cout << time_spent_sec/60 << " min " << time_spent_sec << " sec\n\n";
+}
---
+++ Laba_1/Laba_AиСД_рекурсия.cpp
@@ -0,0 +1,43 @@
+﻿#include<iostream>
+#include<time.h>
+using namespace std;
+
+int Fibonachi(int SIZE, int ansver[], int t);
+
+void main()
+{
+	int SIZE{}, ansver, * fibonachiArray, t{ 2 };
+	cout << "Enter integer N: \t";
+	cin >> SIZE;
+	clock_t start{ clock() };
+	if (SIZE > 0)
+	{
+		fibonachiArray = new int[SIZE];
+		fibonachiArray[0] = 0;
+		fibonachiArray[1] = 1;
+		if (SIZE == 1)
+			cout << fibonachiArray[0];
+		else if (SIZE == 2)
+			cout << fibonachiArray[1];
+		else if (SIZE > 2)
+		{
+			ansver = Fibonachi(SIZE, fibonachiArray, t);
+			cout << ansver << "\n\n\n";
+		}
+		clock_t end{ clock() };
+		double time_spent_sec{ (double)(end - start) / CLOCKS_PER_SEC };
+		cout << time_spent_sec / 60 << " min " << time_spent_sec << " sec\n\n";
+	}
+	else
+		cout << "Error...";
+}
+
+int Fibonachi(int SIZE, int ansver[], int t)
+{
+	ansver[t] = ansver[t - 1] + ansver[t - 2];
+	t++;
+	if (t == SIZE)
+		return ansver[t - 1];
+	else
+		return Fibonachi(SIZE, ansver, t);
+}
